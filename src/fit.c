0570abb8576342ea3541c92d275a6aa80cd11492
NameforWRS <38657662+NameforWRS@users.noreply.github.com>
2018-11-06 17:29:19 +0800

    Update fit.c

--- src/fit.c
+++ src/fit.c
@@ -11,14 +11,14 @@ static double *wtsqrsums, *wttrsqrsums;
 static double *wts, *trs, *trsums;
 static int *countn;
 static int *tsplit;
-Rprintf("fit.c\n");
+
 
 int
 fitinit(int n, double *y[], int maxcat, char **error,
         int *size, int who, double *wt, double *treatment, int bucketnum,
         int bucketMax, double *train_to_est_ratio)
 {
-	
+	Rprintf("fit.c\n");
 	
     if (who == 1 && maxcat > 0) {
         graycode_init0(maxcat);
